a6b5e4707184a71e7601c89033884fdb4940bdf5
Arkovean <anatoliy.arkove@gmail.com>
2023-08-03 07:34:56 +0300

    Error Handling Cleanups

--- SqlUniversity/Controllers/UniversityControllerBase.cs
+++ SqlUniversity/Controllers/UniversityControllerBase.cs
@@ -6,39 +6,6 @@ namespace SqlUniversity.Controllers
 {
     public class UniversityControllerBase : ControllerBase
     {
-        protected async Task<TResponse> ErrorWrapper<TRequest, TResponse>(Func<Task<TResponse>> callback, Func<TRequest> exceptionCallback = null) where TResponse : UniversityReponseBase<TRequest>, new()
-                                                                                                          where TRequest : class
-        {
-            TResponse response = null;
-            try
-            {
-                response = await callback();
-            }
-            catch (UninversityException universityException)
-            {
-                response = new TResponse
-                {
-                    ErrorSection = new ErrorSection
-                    {
-                        Message = "There was a problem please resolve it",
-                        Errors = universityException.Errors
-
-                    },
-                    Request = exceptionCallback?.Invoke(),
-                    IsOperationPassed = false
-                };
-            }
-            catch (Exception ex)
-            {
-                response = new TResponse();
-                response.ErrorSection = new ErrorSection { Message = ex.Message };
-                response.IsOperationPassed = false;
-            }
-
-
-            return response;
-        }
-
         protected async Task<TResponse> ErrorWrapper<TRequest, TResponse>( TRequest request, Func<TRequest,Task<TResponse>> callback) where TResponse : UniversityReponseBase<TRequest>, new()
                                                                                                           where TRequest : class
         {
